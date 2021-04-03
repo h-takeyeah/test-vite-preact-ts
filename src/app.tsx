9b6d92eafe02f9d8e73a5dc82be1a5ffa99da9c9
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-04-03 16:44:18 +0900

    Make App interactive

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,28 @@
 import { Component } from 'preact'
 
 export class App extends Component {
+  // こいつらはクラスのメンバなのでconstやletをつけない
+  state = { value: '', name: 'world' }
+
+  onInput = (ev: Event) => {
+    // as HTMLInputElement はtargetにvalueが存在しませんと言われたから
+    this.setState({ value: (ev.target as HTMLInputElement).value })
+  }
+
+  onSubmit = (ev: Event) => {
+    ev.preventDefault()
+    this.setState({ name: this.state.value })
+  }
+
   render() {
-    return <h1>Hello World!</h1>
+    return (
+      <div>
+        <h1>Hello, {this.state.name}!</h1>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.value} onInput={this.onInput} />
+          <button type="submit">Update</button>
+        </form>
+      </div>
+    )
   }
 }
