e0e849fe93c0aa14ce1e44b6b1e752d97ca4104d
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-04-03 19:50:37 +0900

    Add componentDidMount and so on

--- src/app.tsx
+++ src/app.tsx
@@ -30,6 +30,29 @@ export class App extends Component {
 }
 
 class Clock extends Component {
+  state = { time: Date.now() }
+  /**
+   * This is {@link https://developer.mozilla.org/ja/docs/Web/API/WindowOrWorkerGlobalScope/setInterval intervalID}
+   *
+   * (オマケ)`!`は *definite assignment assertion operator* で、
+   * この変数は(たぶん参照時の)non-nullが保証されているよとコンパイラに教えてあげるやつ
+   *
+   * {@link https://typescript-jp.gitbook.io/deep-dive/intro/strictnullchecks#definite-assignment-assertion-yan-suan-zi-definite-assignment-assertion-operator Deep Dive}
+   *
+   * {@link https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-7.html#strict-class-initialization TS2.7リリースノート}
+   */
+  timer!: number
+
+  componentDidMount () {
+    this.timer = setInterval(() => {
+      this.setState({ time: Date.now() })
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
   render () {
     let time = new Date().toLocaleTimeString()
     return <span>{time}</span>
--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,23 @@ import { render } from 'preact'
 import { App } from './app'
 import './index.css'
 
+//render(App, document.getElementById('app')!) // エラーは出ないが描画されず
+//render(App, document.body)                   // 上に同じ
+
+//h(App,null)とかしてみた(そもそも(Class)Componentのrender()のreturnでJSXの代わりに
+//書くものだから使い方違うけど)
+//そしたら
+//Internal server error: unknown: Identifier 'h' has already been declared
+//が出た。原因はhが自動でinjectionされていることらしい。vite.config.ts内pluginにpreact
+//のpluginを指定するとこうなる。
+//今回は"npm init @vitejs/app test-vite-preact-ts -- --template preact-ts"で
+//プロジェクトを作ったので最初からpluginにpreactが指定されていた。だから
+//import { h, render } from 'preact'
+//みたいに明示的にimportすると今のエラーが出る。
+
+//とりあえず人間はJSXで書けばオールOKでpreact向けのh形式への変換だり何だりは全て機械がやってくれる
+//ので触らんとこって感じなんだろう。ちなみに純粋なprect-tsプロジェクトでhのimportを省略したら
+//エラーになったのでhが自動でinjectionされるのはvite固有の動き
+// ref. https://preactjs.com/guide/v10/differences-to-react#jsx-constructor
+
 render(<App />, document.getElementById('app')!) // !が必要なのマジで謎
