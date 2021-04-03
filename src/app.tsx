4fb2f8a10b98098e591cdbfb75e4c29a8c15d95b
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-04-03 15:59:36 +0900

    Make App Class Component

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,8 @@
 // import { Logo } from './logo'
+import { Component } from 'preact'
 
-export const App = <h1>Hello World!</h1>
+export class App extends Component {
+  render() {
+    return <h1>Hello World!</h1>
+  }
+}
